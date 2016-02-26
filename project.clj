79dad7b2ddb2a0032aade0358a1a1e9e5ee6fce4
Michal Lopuszynski <lopusz@users.noreply.github.com>
2016-02-26 15:22:18 +0100

    Adding project.clj for Leiningen builder

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject com.jhlabs/javaproj "1.1.0-RC1"
+
+  ; GENERAL OPTIONS
+
+  :description "description"
+  :url "url"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/"}
+  :omit-source true
+
+  :source-paths [ ]
+  :java-source-paths ["src/"]
+  
+  ; DEPENDENCIES
+
+  :dependencies []
+
+)
